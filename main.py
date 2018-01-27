1401c988b757b534499608e09dd9a5c62e8abb7a
Jinsung Ha <jsung5381@naver.com>
2018-01-27 14:51:52 +0000

    Twitter steaming is working, currently accepting real-time tweets

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ import json
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 from config import ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter_stream = TwitterStream(auth=oauth)
+
+iterator = twitter_stream.statuses.sample()
+
+tweet_count = 1000
+for tweet in iterator:
+    tweet_count -= 1
+    print (json.dumps(tweet))
+
+    if tweet_count <= 0:
+        break
 
