099fbcb911b2d4431f9b68c4d4c9e1b78a510efd
Jinsung Ha <jsung5381@naver.com>
2018-01-27 17:34:02 +0000

    Implemented Twitter direct_message sender

---
+++ sender.py
@@ -0,0 +1,16 @@
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+from config import ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET
+
+class TwitterSender():
+    
+    def __init__(self, ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET):
+        oAuth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+        self.twitter =  Twitter(auth=oAuth)
+
+    def send_message(self, user, text):
+        self.twitter.direct_messages.new(user=user, text=text)
+
+if __name__ == "__main__":
+    t = TwitterSender(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+    t.send_message('twit_reva', '!!')
+
