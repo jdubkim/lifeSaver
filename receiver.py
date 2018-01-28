321e154766fc8df86ea79fa7b74d498941a98dc1
Jinsung Ha <jsung5381@naver.com>
2018-01-28 01:14:40 +0000

    The receiver now receives tweets that are written in English only

--- receiver.py
+++ receiver.py
@@ -1,4 +1,5 @@
 import json
+
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 from config import ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
@@ -11,29 +12,39 @@ class TwitterReceiver():
         self.twitter_stream = TwitterStream(auth=oAuth)
 
     def receive_tweets(self, nb_items):
+        # Read data from json
+        fn = 'data.json'
+        try:
+            f = open(fn, 'r')
+        except:
+            f = open(fn, 'w')
+            f.write('{}')
+            f.close()
+            f = open(fn, 'r')
+
+        json_data = json.load(f)
+        f.close()
+
+        # Set flag isEmpty
+        isEmpty = True if json_data == {} else False 
+
         # Set an iterator which iterates all visible tweets on Twitter
         iterator = self.twitter_stream.statuses.sample()
-        isEmpty = False
-
-        with open("data.json", 'r') as f:
-            json_data = json.load(f)
-            if json_data == {}:
-                isEmpty = True
 
         # Iterate the tweets with index
         for idx, tweet in enumerate(iterator):
             if idx == nb_items:
                 break
+            elif 'created_at' not in tweet:
+                print('The signal is tweet deletion')
+                continue
+            elif not tweet['user']['lang'] == 'en':
+                continue
             else:
-                if "created_at" in tweet:
-                    if not isEmpty:
-                        json_data[tweet["user"]["name"]] = {"user": tweet["user"],
-                                                        "text": tweet["text"]}
-                    else:
-                        json_data[tweet["user"]["name"]] = {"user": tweet["user"],
-                                                        "text": tweet["text"]}
+                if not isEmpty:
+                    json_data[tweet["user"]["name"]] = {"user": tweet["user"], "text": tweet["text"]}
                 else:
-                    print("The signal is tweet deletion.")
+                    json_data[tweet["user"]["name"]] = {"user": tweet["user"], "text": tweet["text"]}
 
         with open("data.json", 'w') as f:
             json.dump(json_data, f, indent=2, ensure_ascii=False)
@@ -54,5 +65,4 @@ if __name__ == "__main__":
     t = TwitterReceiver(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
     t.receive_tweets(5)
     #t.pop_sent_user("Patrick")
-
 
