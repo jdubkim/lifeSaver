00962cb9fb05b5ca1d3fa2c7a84ba2dc8e367917
Jinsung Ha <jsung5381@naver.com>
2018-01-28 00:13:14 +0000

    Implemented Twitter sender

--- sender.py
+++ sender.py
@@ -10,7 +10,14 @@ class TwitterSender():
     def send_message(self, user, text):
         self.twitter.direct_messages.new(user=user, text=text)
 
+    def send_image(self, user, image):
+        with open(image, 'rb') as img:
+           img_data= img.read() 
+           params = {"media[]": img_data, "status": '@{} '.format(user)}
+           self.twitter.statuses.update_with_media(**params)
+
 if __name__ == "__main__":
     t = TwitterSender(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
-    t.send_message('@twit_reva', '!!')
+    #t.send_message('twit_reva', '!!')
+    t.send_image('twit_reva', 'test.png')
 
